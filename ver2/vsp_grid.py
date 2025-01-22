61e4fdb4d05a52a23d5ca11765cf33697fded6e3
Gyeong-Rak <rudfkr5978@naver.com>
2025-01-22 21:09:10 +0900

    bug fix

--- ver2/vsp_grid.py
+++ ver2/vsp_grid.py
@@ -47,7 +47,7 @@ def runVSPGridAnalysis(aircraftParamConstraint: AircraftParamConstraints,presetV
         total_combinations = np.prod([len(arr) for arr in [span_list,AR_list,taper_list,twist_list]])
 
         for i, (span, AR, taper, twist, m_total) in enumerate(product(span_list,AR_list,taper_list,twist_list,total_mass_list)):
-                print(f"[{time.strftime("%Y-%m-%d %X")}] Progress: {i}/{total_combinations} configurations")
+                print(f"[{time.strftime('%Y-%m-%d %X')}] Progress: {i}/{total_combinations} configurations")
                 aircraft = replace(baseAircraft, mainwing_span = span, mainwing_AR = AR , mainwing_taper = taper, mainwing_twist = twist, m_total = m_total)   
 
         vspAnalyzer.setup_vsp_model(aircraft)
