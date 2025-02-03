4b69357f674e6421879750f82984c5aadb659479
Jaewon Chung <jaewonchung7@gmail.com>
2025-02-03 18:15:25 +0900

    Added installDocker script
    Signed-off-by: Jaewon Chung <jaewonchung7@gmail.com>

---
+++ ver2/installDocker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install -y docker
+
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.32.4/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
